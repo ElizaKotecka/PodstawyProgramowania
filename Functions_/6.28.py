4a797fac4fb1123e8a5127c258546228c9d99765
ElizaGancarz <elizaganca@gmail.com>
2024-12-12 12:34:56 +0100

    finished functions

--- Functions_/6.28.py
+++ Functions_/6.28.py
@@ -1,7 +1,24 @@
 # Returns a number specifying the number of dice rolled the most times in a row.
 
 def f(dice):
-    pass
+    prev_number = dice[0] # Remember previous number
+    counter = 1
+    max_len_of_sequence = 1 
+    num_in_max_len = dice[0] # Number in longest sequence
+    for x in dice[1:]:
+        if x == prev_number:
+            counter += 1
+        else:
+            if max_len_of_sequence < counter:
+                max_len_of_sequence = counter
+                num_in_max_len = prev_number
+                counter = 1
+        prev_number = x
+    if max_len_of_sequence < counter: # If the longest sequence at the end
+        max_len_of_sequence = counter
+        num_in_max_len = prev_number
+    return num_in_max_len
+
 
 # Test cases
 print(f("5233165554211"))  # Expected output: 5
--- Functions_/6.31.py
+++ Functions_/6.31.py
@@ -1,9 +1,9 @@
 # Define a function power(x, n) that calculates xn. Apply recursion. Then, calculate 53.
 
 def power (x, n):
-    if n == 1:
+    if n == 0:
         return 1
-    return x * x**(n-1)
+    return x * power(x, n-1) #Tip: x**n = x * x**(n-1)
 
 if __name__ == '__main__':
     print(power(5, 3))
